448770d1df84dc8ccbec512984a2650e602b4b90
GoG <forsoapfc@gmail.com>
2021-01-06 14:57:57 +0800

    Added Explanation session

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 interface Props {
   singleData: any;
@@ -6,6 +6,7 @@ interface Props {
 
 const Question = (props: Props) => {
   const tableRef = useRef<HTMLTableElement>(null);
+  const [showExplanation, setShowExplanation] = useState(false);
 
   let currentSelection: { [key: string]: boolean } = {}; // index signature
 
@@ -33,6 +34,7 @@ const Question = (props: Props) => {
         if (data.answers[key]) {
           currentSelection[key] = false;
         }
+        return null;
       });
     }
   }, [props.singleData]);
@@ -53,12 +55,13 @@ const Question = (props: Props) => {
     let i = 0;
     if (data.correct_answers) {
       Object.keys(data.correct_answers).map((key) => {
-        if (data.correct_answers[key] == "true") {
+        if (data.correct_answers[key] === "true") {
           correct[i] = true;
         } else {
           correct[i] = false;
         }
         i++;
+        return null;
       });
 
       console.log(correct);
@@ -69,12 +72,16 @@ const Question = (props: Props) => {
       Object.keys(data.answers).map((key) => {
         if (currentSelection[key]) {
           if (currentSelection[key] !== correct[j]) {
+            // TODO: consider use data.multiple_correct_answers: false (is a string) ?
             console.log("CS: " + currentSelection[key]);
             console.log("C[" + j + "]: " + correct[j]);
             console.log("Incorrect Answer Index: " + j);
+            console.log(data);
+            setShowExplanation(true);
           }
           j++;
         }
+        return null;
       });
     }
   }
@@ -123,6 +130,23 @@ const Question = (props: Props) => {
             </tbody>
           </table>
           <div>
+            {showExplanation && (
+              // This api have mostly none explanation
+              <div>
+                <h4>Explanation: </h4>
+                {props.singleData.explanation ? (
+                  <p>{props.singleData.explanation}</p>
+                ) : (
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </p>
+                )}
+              </div>
+            )}
             <button onClick={checkAnswers}>Submit</button>
           </div>
         </div>
