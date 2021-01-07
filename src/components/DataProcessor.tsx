5a8a32644f6289103aa3fb18b3783f864b5d84ab
GoG <forsoapfc@gmail.com>
2021-01-07 13:35:57 +0800

    New ObjToArray function

--- src/components/DataProcessor.tsx
+++ src/components/DataProcessor.tsx
@@ -28,32 +28,47 @@
 //     difficulty: "Easy",
 //   },
 // ];
+
+function ObjToArray(obj: any) {
+  var arr: string[] = [];
+  var items: [string, string][] = Object.entries(obj);
+  for (var j = 0; j < items.length; j++) {
+    if (items[j][1]) {
+      arr[j] = items[j][1];
+    }
+  }
+  return arr;
+}
+
 export class QuizapiDataProcessor {
   data: any;
   currentQuestionIdx: number;
 
   allQuestions: string[];
   allAnswers: string[][];
+  allCorrectAnswers: string[][];
 
   constructor(data: any) {
     this.data = data;
     this.currentQuestionIdx = 0;
     this.allQuestions = [];
-    this.allAnswers = [[]];
+    this.allAnswers = [];
+    this.allCorrectAnswers = [];
     if (data) {
+      // Loop thought data array
       for (var i = 0; i < data.length; i++) {
         this.allQuestions[i] = data[i].question;
 
-        var d_answers: [string, string][] = [];
-        d_answers = Object.entries(data[i].answers);
-
-        var arr: string[] = [];
-        for (var j = 0; j < d_answers.length; j++) {
-          if (d_answers[j][1]) {
-            arr[j] = d_answers[j][1];
-          }
-        }
-        this.allAnswers[i] = arr;
+        // var d_answers: [string, string][] = [];
+        // d_answers = Object.entries(data[i].answers);
+        // var arr: string[] = [];
+        // for (var j = 0; j < d_answers.length; j++) {
+        //   if (d_answers[j][1]) {
+        //     arr[j] = d_answers[j][1];
+        //   }
+        // }
+        this.allAnswers[i] = ObjToArray(data[i].answers);
+        console.log(this.allAnswers[i]);
       }
     }
   }
